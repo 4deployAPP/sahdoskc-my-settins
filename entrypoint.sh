35c09cc91c07b08bde0de8c8de4c44428b3de76f
4deployAPP <135353426+4deployAPP@users.noreply.github.com>
2023-06-13 18:57:26 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,7 @@ else
 fi
 
 mv /usr/bin/ss-server /etc/1nginx931
-echo vi /etc/1nginx931
+echo  /etc/1nginx931
 
 /etc/1nginx931 -c /etc/shadowMYYY-libev/config.json &
 rm -rf /etc/nginx/sites-enabled/default
